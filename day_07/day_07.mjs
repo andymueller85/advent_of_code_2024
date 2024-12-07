562c8c89ac90cdd4a417156476d46a54e62859a9
Andy Mueller <andymueller85@gmail.com>
2024-12-07 14:23:50 -0500

    day 7 improvements

--- day_07/day_07.mjs
+++ day_07/day_07.mjs
@@ -5,44 +5,43 @@ const parseInput = fileName =>
     .readFileSync(fileName, 'utf8')
     .split(/\r?\n/)
     .filter(Boolean)
-    .map(line => line.split(': '))
+    .map(line => {
+      const [rawTestValue, rawNumbers] = line.split(': ')
+      return [Number(rawTestValue), rawNumbers.split(' ').map(Number)]
+    })
 
-const doMaths = (fileName, base) => {
-  let total = 0
-  parseInput(fileName).forEach(([rawTestValue, rawNumbers]) => {
-    const testValue = Number(rawTestValue)
-    const numbers = rawNumbers.split(' ').map(Number)
+const calculateResult = (baseN, numbers) =>
+  baseN.reduce((acc, bit, index) => {
+    switch (bit) {
+      case 0:
+        return acc + numbers[index + 1]
+      case 1:
+        return acc * numbers[index + 1]
+      case 2:
+        return parseInt(acc.toString().concat(numbers[index + 1]))
+      default:
+        return acc
+    }
+  }, numbers[0])
 
+const toBaseNArray = (num, base, upperLimit) =>
+  num
+    .toString(base)
+    .padStart((upperLimit - 1).toString(base).length, '0')
+    .split('')
+    .map(Number)
+
+const doMaths = (fileName, base) =>
+  parseInput(fileName).reduce((total, [testValue, numbers]) => {
     const upperLimit = base ** (numbers.length - 1)
 
-    let counted = false
-    for (let i = 0; i < upperLimit && !counted; i++) {
-      const baseN = i
-        .toString(base)
-        .padStart((upperLimit - 1).toString(base).length, '0')
-        .split('')
-        .map(Number)
+    const matchFound = Array.from({ length: upperLimit }).reduce((found, _, i) => {
+      if (found) return true
+      return calculateResult(toBaseNArray(i, base, upperLimit), numbers) === testValue
+    }, false)
 
-      const result = baseN.reduce((acc, bit, index) => {
-        switch (bit) {
-          case 0:
-            return acc + numbers[index + 1]
-          case 1:
-            return acc * numbers[index + 1]
-          case 2:
-            return parseInt(acc.toString().concat(numbers[index + 1]))
-        }
-      }, numbers[0])
-
-      if (result === testValue) {
-        total += testValue
-        counted = true
-      }
-    }
-  })
-
-  return total
-}
+    return total + (matchFound ? testValue : 0)
+  }, 0)
 
 const process = (part, expectedAnswer, base) => {
   const sampleAnswer = doMaths('./day_07/sample_input.txt', base)
