bd2ef48ab1b29d2d7e57e147218b25533e076b7d
Andy Mueller <andymueller85@gmail.com>
2024-12-12 11:38:50 -0500

    day 12 part 1

---
+++ day_12/day_12.mjs
@@ -0,0 +1,81 @@
+import * as fs from 'fs'
+import { process } from '../utils.mjs'
+
+const parseInput = fileName =>
+  fs
+    .readFileSync(fileName, 'utf8')
+    .split(/\r?\n/)
+    .filter(Boolean)
+    .map(row => row.split(''))
+
+const isInBounds = (grid, rowI, colI) =>
+  rowI >= 0 && rowI < grid.length && colI >= 0 && colI < grid[0].length
+
+const getPerimeter = (plot, grid) =>
+  Array.from(plot).reduce((perimeter, cellCoords) => {
+    const [rowI, colI] = cellCoords.split(',').map(Number)
+
+    return (
+      perimeter +
+      [
+        [rowI, colI - 1], // left
+        [rowI - 1, colI], // up
+        [rowI, colI + 1], // right
+        [rowI + 1, colI] // down
+      ].filter(
+        ([candidateRowI, candidateColI]) =>
+          !isInBounds(grid, candidateRowI, candidateColI) ||
+          !plot.has(`${candidateRowI},${candidateColI}`)
+      ).length
+    )
+  }, 0)
+
+const findAdjacentPlants = (grid, rowI, colI, curPlot, plottedCells) => {
+  const cell = grid[rowI][colI]
+
+  ;[
+    [rowI, colI - 1], // left
+    [rowI - 1, colI], // up
+    [rowI, colI + 1], // right
+    [rowI + 1, colI] // down
+  ].forEach(([candidateRowI, candidateColI]) => {
+    if (!isInBounds(grid, candidateRowI, candidateColI)) return
+
+    const candidateNeighbor = grid[candidateRowI][candidateColI]
+    const candidateNeighborCoords = `${candidateRowI},${candidateColI}`
+
+    if (
+      !plottedCells.has(candidateNeighborCoords) &&
+      !curPlot.has(candidateNeighborCoords) &&
+      candidateNeighbor === cell
+    ) {
+      curPlot.add(candidateNeighborCoords)
+      plottedCells.add(candidateNeighborCoords)
+      curPlot = findAdjacentPlants(grid, candidateRowI, candidateColI, curPlot, plottedCells)
+    }
+  })
+
+  return curPlot
+}
+
+const createPlots = (grid, plotted) =>
+  grid.reduce((plots, row, rowI) => {
+    row.forEach((_, colI) => {
+      if (!plotted.has(`${rowI},${colI}`)) {
+        // if a cell hasn't been visited, start a new plot and recursively find adjacent plants
+        plots.push(findAdjacentPlants(grid, rowI, colI, new Set([`${rowI},${colI}`]), plotted))
+      }
+    })
+    return plots
+  }, [])
+
+const partA = fileName => {
+  const grid = parseInput(fileName)
+
+  return createPlots(grid, new Set()).reduce(
+    (acc, cur) => acc + cur.size * getPerimeter(cur, grid),
+    0
+  )
+}
+
+process(12, 'A', 1930, partA)
