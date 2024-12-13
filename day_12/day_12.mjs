5bb08d5b4dc5bb7f5e32ca027b9fec30e2b5bad2
Andy Mueller <andymueller85@gmail.com>
2024-12-12 20:14:12 -0500

    day 12 part 2

--- day_12/day_12.mjs
+++ day_12/day_12.mjs
@@ -12,23 +12,95 @@ const isInBounds = (grid, rowI, colI) =>
   rowI >= 0 && rowI < grid.length && colI >= 0 && colI < grid[0].length
 
 const getNeighbors = (rowI, colI) => [
-  [rowI, colI - 1], // left
-  [rowI - 1, colI], // up
-  [rowI, colI + 1], // right
-  [rowI + 1, colI] // down
+  [rowI, colI - 1], // west
+  [rowI - 1, colI], // north
+  [rowI, colI + 1], // east
+  [rowI + 1, colI] // south
 ]
+
+const createPerimeterCellChecker =
+  (plot, grid) =>
+  ([candidateRowI, candidateColI]) =>
+    !isInBounds(grid, candidateRowI, candidateColI) ||
+    !plot.has(`${candidateRowI},${candidateColI}`)
 
 const getPerimeter = (plot, grid) =>
   Array.from(plot).reduce(
     (perimeter, cellCoords) =>
       perimeter +
       getNeighbors(...cellCoords.split(',').map(Number)).filter(
-        ([candidateRowI, candidateColI]) =>
-          !isInBounds(grid, candidateRowI, candidateColI) ||
-          !plot.has(`${candidateRowI},${candidateColI}`)
+        createPerimeterCellChecker(plot, grid)
       ).length,
     0
   )
+
+const getPlotPerimeters = (plot, grid) =>
+  Array.from(plot).map(cellCoords => {
+    const [rowI, colI] = cellCoords.split(',').map(Number)
+    const [west, north, east, south] = getNeighbors(rowI, colI)
+
+    const isPerimeterCell = createPerimeterCellChecker(plot, grid)
+
+    return {
+      cellCoords: [rowI, colI],
+      west: isPerimeterCell(west),
+      north: isPerimeterCell(north),
+      east: isPerimeterCell(east),
+      south: isPerimeterCell(south)
+    }
+  })
+
+// return count of contiguous groups of values in the array
+// [0,1,3,4] => 2
+const getPerimeterCountForRowOrColumn = rowOrCol =>
+  rowOrCol
+    .sort((a, b) => a - b)
+    .reduce((acc, cur, i) => {
+      if (i === 0 || cur - rowOrCol[i - 1] > 1) {
+        return acc + 1
+      }
+
+      return acc
+    }, 0)
+
+const getPerimetersByRowOrCol = (plotPerimeters, direction) =>
+  plotPerimeters.reduce((acc, cur) => {
+    const index = ['north', 'south'].includes(direction) ? 0 : 1
+    const key = cur.cellCoords[index]
+
+    if (acc[key] === undefined) {
+      acc[key] = { [direction]: key, vals: [] }
+    }
+
+    if (cur[direction]) {
+      acc[key].vals.push(cur.cellCoords[1 - index])
+    }
+
+    return acc
+  }, [])
+
+const getPerimeterTotal = (plotPerimeters, direction) => {
+  const perimetersByRowOrCol = getPerimetersByRowOrCol(plotPerimeters, direction)
+  return perimetersByRowOrCol
+    .map(rowOrCol => getPerimeterCountForRowOrColumn(rowOrCol.vals))
+    .reduce((acc, cur) => acc + cur, 0)
+}
+
+const getStraightEdgesCount = (plot, grid) => {
+  // - for each cell of the grid, determine if it's on the perimeter of the plot, and in which direction(s)
+  // - once we have the full list of perimeters, we can find any gaps along straight
+  //     lines in both directions (north/south for horizontal, east/west for vertical)
+  // - count up the edges in each direction
+
+  const plotPerimeters = getPlotPerimeters(plot, grid)
+
+  return (
+    getPerimeterTotal(plotPerimeters, 'north') +
+    getPerimeterTotal(plotPerimeters, 'south') +
+    getPerimeterTotal(plotPerimeters, 'east') +
+    getPerimeterTotal(plotPerimeters, 'west')
+  )
+}
 
 const findAdjacentPlants = (grid, rowI, colI, curPlot, plottedCells) => {
   const cell = grid[rowI][colI]
@@ -44,8 +116,8 @@ const findAdjacentPlants = (grid, rowI, colI, curPlot, plottedCells) => {
       !curPlot.has(candidateNeighborCoords) &&
       candidateNeighbor === cell
     ) {
-      curPlot.add(candidateNeighborCoords)
       plottedCells.add(candidateNeighborCoords)
+      curPlot.add(candidateNeighborCoords)
       curPlot = findAdjacentPlants(grid, candidateRowI, candidateColI, curPlot, plottedCells)
     }
   })
@@ -73,4 +145,16 @@ const partA = fileName => {
   )
 }
 
+const partB = fileName => {
+  const grid = parseInput(fileName)
+  const plotted = new Set()
+  const plots = createPlots(grid, plotted)
+
+  return plots.reduce(
+    (totalPrice, plot) => totalPrice + plot.size * getStraightEdgesCount(plot, grid),
+    0
+  )
+}
+
 process(12, 'A', 1930, partA)
+process(12, 'B', 1206, partB)
