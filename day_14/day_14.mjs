110a61a61384cc9becd82849f9712c01e49deb5b
Andy Mueller <andymueller85@gmail.com>
2024-12-14 14:34:53 -0500

    day 14 part 2

--- day_14/day_14.mjs
+++ day_14/day_14.mjs
@@ -28,46 +28,69 @@ const reposition = (robot, gridWidth, gridHeight) => {
   return { position: [newCol, newRow], velocity }
 }
 
-const partA = (fileName, gridWidth, gridHeight) => {
+const printGrid = (robots, gridWidth, gridHeight, i) => {
+  const grid = Array.from({ length: gridHeight }, () =>
+    Array.from({ length: gridWidth }, () => '.')
+  )
+
+  let middleCount = 0
+  robots.forEach(({ position: [col, row] }) => {
+    grid[row][col] = '#'
+
+    // count the number of robots towards the center
+    if (
+      row >= gridWidth / 3 &&
+      row <= (gridWidth * 2) / 3 &&
+      col >= gridHeight / 3 &&
+      col <= (gridHeight * 2) / 3
+    )
+      middleCount++
+  })
+
+  if (middleCount > 200) {
+    console.log(`time ${i + 1}`)
+
+    grid.forEach(row => console.log(row.join('')))
+    console.log()
+  }
+}
+
+const arrangeRobots = (fileName, w, h, loopCtr) => {
+  const topLeft = robot =>
+    robot.position[0] < Math.floor(w / 2) && robot.position[1] < Math.floor(h / 2)
+  const topRight = robot => robot.position[0] > w / 2 && robot.position[1] > h / 2
+  const bottomLeft = robot => robot.position[0] < Math.floor(w / 2) && robot.position[1] > h / 2
+  const bottomRight = robot => robot.position[0] > w / 2 && robot.position[1] < Math.floor(h / 2)
+
   let robots = parseInput(fileName)
 
-  for (let i = 0; i < 100; i++) {
-    robots = robots.map(robot => reposition(robot, gridWidth, gridHeight))
+  for (let i = 0; i < loopCtr; i++) {
+    robots = robots.map(robot => reposition(robot, w, h))
+
+    printGrid(robots, w, h, i)
   }
 
-  const topLeftQuadrantCount = robots.filter(
-    robot =>
-      robot.position[0] < Math.floor(gridWidth / 2) &&
-      robot.position[1] < Math.floor(gridHeight / 2)
-  ).length
-  const topRightQuadrantCount = robots.filter(
-    robot => robot.position[0] > gridWidth / 2 && robot.position[1] > gridHeight / 2
-  ).length
-  const bottomLeftQuadrantCount = robots.filter(
-    robot => robot.position[0] < Math.floor(gridWidth / 2) && robot.position[1] > gridHeight / 2
-  ).length
-  const bottomRightQuadrantCount = robots.filter(
-    robot => robot.position[0] > gridWidth / 2 && robot.position[1] < Math.floor(gridHeight / 2)
-  ).length
-
   return (
-    topLeftQuadrantCount *
-    topRightQuadrantCount *
-    bottomLeftQuadrantCount *
-    bottomRightQuadrantCount
+    robots.filter(topLeft).length *
+    robots.filter(topRight).length *
+    robots.filter(bottomLeft).length *
+    robots.filter(bottomRight).length
   )
 }
 
-export const process = (day, part, expectedAnswer, fn) => {
+export const process = (day, expectedAnswer, fn) => {
   const strDay = day.toString().padStart(2, '0')
-  const sampleAnswer = fn(`./day_${strDay}/sample_input.txt`, 11, 7)
+  const sampleAnswer = fn(`./day_${strDay}/sample_input.txt`, 11, 7, 100)
 
-  console.log(`part ${part} sample answer`, sampleAnswer)
+  console.log('part A sample answer', sampleAnswer)
   if (sampleAnswer !== expectedAnswer) {
-    throw new Error(`part ${part} sample answer should be ${expectedAnswer}`)
+    throw new Error(`part A sample answer should be ${expectedAnswer}`)
   }
 
-  console.log(`part ${part} real answer`, fn(`./day_${strDay}/input.txt`, 101, 103))
+  console.log('part A real answer', fn(`./day_${strDay}/input.txt`, 101, 103, 100))
+
+  console.log('Part B...')
+  console.log(fn(`./day_${strDay}/input.txt`, 101, 103, 10000))
 }
 
-process(14, 'A', 12, partA)
+process(14, 12, arrangeRobots)
